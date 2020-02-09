d8715ccca078858905d9943f95d5c22bddad78c6
Locketgoma <gom@kiwing.co.kr>
2020-02-10 01:56:28 +0900

    얘는 도대체 구조가 왜 이런것인가

--- MultiadapterTwit.cpp
+++ MultiadapterTwit.cpp
@@ -3,8 +3,29 @@
 
 //Public
 
-MultiadapterTwit::MultiadapterTwit(int width, int height, LPCWSTR name) {
-
-
-
+//아니 이거 구조 왜이래?
+MultiadapterTwit::MultiadapterTwit(int width, int height, LPCWSTR name) :
+    DXSample(width, height, name),
+    m_frameIndex(0),
+    m_BirdCount(MaxBirdCount / 2),
+    m_psLoopCount(0),
+    m_blurPSLoopCount(0),
+    m_currentTimesIndex(0),
+    m_drawTimeMovingAverage(0),
+    m_blurTimeMovingAverage(0),
+    m_viewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height)),
+    m_scissorRect(0, 0, static_cast<LONG>(width), static_cast<LONG>(height)),
+    m_currentPresentFenceValue(1),
+    m_currentRenderFenceValue(1),
+    m_currentCrossAdapterFenceValue(1),
+    m_workloadConstantBufferData(),
+    m_blurWorkloadConstantBufferData(),
+    m_crossAdapterTextureSupport(false),
+    m_rtvDescriptorSizes{},
+    m_srvDescriptorSizes{},
+    m_drawTimes{},
+    m_blurTimes{},
+    m_frameFenceValues{}
+{
+    m_constantBufferData.resize(MaxBirdCount);
 }
